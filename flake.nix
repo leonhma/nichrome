f1b8b8c130963258e31bcbdbee6d5fd5f3779f05
Leonhard Masche <leonhardmasche@gmail.com>
2024-10-04 10:45:28 +0200

    mount fails

--- flake.nix
+++ flake.nix
@@ -14,53 +14,54 @@
       nixosModule = { config, lib, pkgs, ... }: {
         config = {
           system.stateVersion = "24.11";
-          networking = { hostName = "nixos"; };
-          system.activationScripts.crostiniSetup = ''
-            # Create empty /etc/gshadow file
-            touch /etc/gshadow
-
-            # Create /home/leonhardmasche/.config/cros-garcon.conf
-            mkdir -p /home/leonhardmasche/.config
-            echo "DisableAutomaticCrosPackageUpdates=false\nDisableAutomaticSecurityUpdates=false" > /home/leonhardmasche/.config/cros-garcon.conf
-          '';
+          networking = { hostName = "nichrome"; };
         };
       };
     in flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        vmTools = pkgs.vmTools;
         basefs = nixos-generators.nixosGenerate {
           inherit system;
           format = "lxc";
           modules = [ nixosModule ];
         };
+        meta = nixos-generators.nixosGenerate {
+          inherit system;
+          format = "lxc-metadata";
+        };
       in {
         formatter = pkgs.nixfmt-classic;
         packages = {
-          lxcImage = pkgs.stdenv.mkDerivation {
+          default = vmTools.runInLinuxVM (pkgs.stdenv.mkDerivation {
             name = "nichrome-${system}";
+            preVM = vmTools.createEmptyImage {
+              size = 16024;
+              fullName = "nichrome-build";
+            };
+            memSize = 2048;
             dontUnpack = true;
-            buildInputs = with pkgs; [ qemu util-linux e2fsprogs ];
+            buildInputs = with pkgs; [ qemu util-linux e2fsprogs fuse-ext2];
             buildPhase = ''
               set -e
-
+              echo "creating image"
               # Create the raw image
-              dd if=/dev/null of=disk.img bs=16M seek=256
+              qemu-img create -f raw disk.img 800M
+              ls -la
               mkfs.ext4 disk.img
-
-              # Populate the image rootfs
               mkdir rootfs
-              ld=$(sudo losetup -f)
-              sudo losetup $ld disk.img
-              sudo mount $ld rootfs
-
+              ls -la
+              df -h
+              pwd
+              echo "starting first unshare"
+              mount -o loop disk.img ./rootfs/
               ls rootfs
               tar xJf ${basefs}/tarball/nixos-system-${system} -C rootfs
               echo "------"
               ls rootfs
+              umount rootfs
+              echo "unshare done"
 
-              sudo umount $ld
-              sudo losetup -d $ld
-                
               # Boot the VM
               qemu-system-x86_64 \
               -m 1024 \S
@@ -69,30 +70,27 @@
               -nographic \
               -no-reboot \
               -drive file=./disk.img,format=raw,if=virtio
+              
+              mount -o loop disk.img rootfs
 
-              ld=$(losetup -f)
-              sudo losetup $ld disk.img
-              sudo mount $ld rootfs
-    
-              # Create /etc/gshadow if it doesn't exist
+              ls rootfs
               touch rootfs/etc/gshadow
-    
-              # Export the modified rootfs
-              ${pkgs.gnutar}/bin/tar -cJf nichrome-${system}.tar.xz -C rootfs .
-    
-              sudo umount $ld
-              sudo losetup -d $ld
+                  
+              echo "------"
+              ls rootfs
 
+              # Export the modified rootfs
+              ${pkgs.gnutar}/bin/tar -cJf nichrome-${system}-rootfs.tar.xz -C rootfs .
+
+              umount rootfs
+      
               rm -rf rootfs disk.img
             '';
             installPhase = ''
               mv nichrome-${system}.tar.xz $out
+              mv ${meta}/tarball/nixos-system-${system}.tar.xz $out/nichrome-${system}-meta.tar.xz
             '';
-          };
-          lxcMeta = nixos-generators.nixosGenerate {
-            inherit system;
-            format = "lxc-metadata";
-          };
+          });
         };
       });
 }
